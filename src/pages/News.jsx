2b49b6fa616e81ebc541e753ef654415f56dda50
faiz832 <faqodefazz@gmail.com>
2025-06-11 10:50:31 +0700

    add footer each page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,12 +1,17 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function News() {
   return (
     <>
+      {/* Navbar */}
       <Navbar />
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-[552px] md:min-h-[calc(100vh-100px)]">
         <h1>News</h1>
       </div>
+
+      {/* Footer */}
+      <Footer />
     </>
   );
 }
