b7138525a933261e8b0e328a899b17413a486399
faiz832 <faqodefazz@gmail.com>
2025-06-11 20:04:59 +0700

    fix contact pattern

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -24,108 +24,103 @@ export default function Contact() {
 
   return (
     <>
-      <div className="relative overflow-hidden">
-        <img src={Pattern} alt="" className="absolute right-0 top-0 w-40 h-auto -rotate-45 -z-1 opacity-10" />
-        <img src={Pattern} alt="" className="absolute left-0 top-0 w-40 h-auto rotate-45 -z-1 opacity-10" />
+      <div className="contact section max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-center relative">
+          <img src={Pattern3} alt="" className="absolute right-0 top-1/2 transform -translate-y-1/2  opacity-20" />
+          <h1 className="text-6xl w-max mx-auto font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-700 to-green-400 my-12">Contact Us</h1>
+          <img src={Pattern2} alt="" className="absolute left-0 top-1/2 transform -translate-y-1/2  opacity-20" />
+        </div>
+        <p className="w-full lg:w-2/4 mx-auto text-center text-lg">Get in touch with us and let us know how we can help you.</p>
 
-        <div className="contact section max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="relative text-6xl w-max mx-auto font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-700 to-green-400 my-12">
-            <img src={Pattern2} alt="" className="absolute top-0 -right-28 w-24 opacity-20" />
-            Contact Us
-            <img src={Pattern3} alt="" className="absolute top-0 -left-28 w-24 opacity-20" />
-          </div>
-          <p className="w-full lg:w-2/4 mx-auto text-center text-lg">Get in touch with us and let us know how we can help you.</p>
+        <div className="contact-container grid mt-12">
+          <div className="contact-content">
+            <div className="contact-info">
+              <div className="contact-card">
+                <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mx-auto mb-2">
+                  <path
+                    d="M4 18L9 12M20 18L15 12M3 8L10.225 12.8166C10.8665 13.2443 11.1872 13.4582 11.5339 13.5412C11.8403 13.6147 12.1597 13.6147 12.4661 13.5412C12.8128 13.4582 13.1335 13.2443 13.775 12.8166L21 8M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z"
+                    stroke="#07960c"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
 
-          <div className="contact-container grid mt-12">
-            <div className="contact-content">
-              <div className="contact-info">
-                <div className="contact-card">
-                  <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mx-auto mb-2">
-                    <path
-                      d="M4 18L9 12M20 18L15 12M3 8L10.225 12.8166C10.8665 13.2443 11.1872 13.4582 11.5339 13.5412C11.8403 13.6147 12.1597 13.6147 12.4661 13.5412C12.8128 13.4582 13.1335 13.2443 13.775 12.8166L21 8M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z"
-                      stroke="#07960c"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
+                <h3 className="font-semibold font-lexend">Email</h3>
+                <span className="contact-card-data mb-2">kaltimku@gmail.com</span>
+
+                <a href="mailto:kaltimku@gmail.com" className="contact-button mb-2">
+                  Contact us
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#07960c" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M7 7h10v10"></path>
+                    <path d="M7 17 17 7"></path>
                   </svg>
+                </a>
+              </div>
 
-                  <h3 className="font-semibold font-lexend">Email</h3>
-                  <span className="contact-card-data mb-2">kaltimku@gmail.com</span>
+              <div className="contact-card">
+                <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mx-auto mb-2">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M3.50002 12C3.50002 7.30558 7.3056 3.5 12 3.5C16.6944 3.5 20.5 7.30558 20.5 12C20.5 16.6944 16.6944 20.5 12 20.5C10.3278 20.5 8.77127 20.0182 7.45798 19.1861C7.21357 19.0313 6.91408 18.9899 6.63684 19.0726L3.75769 19.9319L4.84173 17.3953C4.96986 17.0955 4.94379 16.7521 4.77187 16.4751C3.9657 15.176 3.50002 13.6439 3.50002 12ZM12 1.5C6.20103 1.5 1.50002 6.20101 1.50002 12C1.50002 13.8381 1.97316 15.5683 2.80465 17.0727L1.08047 21.107C0.928048 21.4637 0.99561 21.8763 1.25382 22.1657C1.51203 22.4552 1.91432 22.5692 2.28599 22.4582L6.78541 21.1155C8.32245 21.9965 10.1037 22.5 12 22.5C17.799 22.5 22.5 17.799 22.5 12C22.5 6.20101 17.799 1.5 12 1.5ZM14.2925 14.1824L12.9783 15.1081C12.3628 14.7575 11.6823 14.2681 10.9997 13.5855C10.2901 12.8759 9.76402 12.1433 9.37612 11.4713L10.2113 10.7624C10.5697 10.4582 10.6678 9.94533 10.447 9.53028L9.38284 7.53028C9.23954 7.26097 8.98116 7.0718 8.68115 7.01654C8.38113 6.96129 8.07231 7.046 7.84247 7.24659L7.52696 7.52195C6.76823 8.18414 6.3195 9.2723 6.69141 10.3741C7.07698 11.5163 7.89983 13.314 9.58552 14.9997C11.3991 16.8133 13.2413 17.5275 14.3186 17.8049C15.1866 18.0283 16.008 17.7288 16.5868 17.2572L17.1783 16.7752C17.4313 16.5691 17.5678 16.2524 17.544 15.9269C17.5201 15.6014 17.3389 15.308 17.0585 15.1409L15.3802 14.1409C15.0412 13.939 14.6152 13.9552 14.2925 14.1824Z"
+                    fill="#07960c"
+                  />
+                </svg>
 
-                  <a href="mailto:kaltimku@gmail.com" className="contact-button mb-2">
-                    Contact us
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#07960c" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M7 7h10v10"></path>
-                      <path d="M7 17 17 7"></path>
-                    </svg>
-                  </a>
-                </div>
+                <h3 className="font-semibold font-lexend">Whatsapp</h3>
+                <span className="contact-card-data mb-2">0811-599-594</span>
 
-                <div className="contact-card">
-                  <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mx-auto mb-2">
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M3.50002 12C3.50002 7.30558 7.3056 3.5 12 3.5C16.6944 3.5 20.5 7.30558 20.5 12C20.5 16.6944 16.6944 20.5 12 20.5C10.3278 20.5 8.77127 20.0182 7.45798 19.1861C7.21357 19.0313 6.91408 18.9899 6.63684 19.0726L3.75769 19.9319L4.84173 17.3953C4.96986 17.0955 4.94379 16.7521 4.77187 16.4751C3.9657 15.176 3.50002 13.6439 3.50002 12ZM12 1.5C6.20103 1.5 1.50002 6.20101 1.50002 12C1.50002 13.8381 1.97316 15.5683 2.80465 17.0727L1.08047 21.107C0.928048 21.4637 0.99561 21.8763 1.25382 22.1657C1.51203 22.4552 1.91432 22.5692 2.28599 22.4582L6.78541 21.1155C8.32245 21.9965 10.1037 22.5 12 22.5C17.799 22.5 22.5 17.799 22.5 12C22.5 6.20101 17.799 1.5 12 1.5ZM14.2925 14.1824L12.9783 15.1081C12.3628 14.7575 11.6823 14.2681 10.9997 13.5855C10.2901 12.8759 9.76402 12.1433 9.37612 11.4713L10.2113 10.7624C10.5697 10.4582 10.6678 9.94533 10.447 9.53028L9.38284 7.53028C9.23954 7.26097 8.98116 7.0718 8.68115 7.01654C8.38113 6.96129 8.07231 7.046 7.84247 7.24659L7.52696 7.52195C6.76823 8.18414 6.3195 9.2723 6.69141 10.3741C7.07698 11.5163 7.89983 13.314 9.58552 14.9997C11.3991 16.8133 13.2413 17.5275 14.3186 17.8049C15.1866 18.0283 16.008 17.7288 16.5868 17.2572L17.1783 16.7752C17.4313 16.5691 17.5678 16.2524 17.544 15.9269C17.5201 15.6014 17.3389 15.308 17.0585 15.1409L15.3802 14.1409C15.0412 13.939 14.6152 13.9552 14.2925 14.1824Z"
-                      fill="#07960c"
-                    />
+                <a href="https://api.whatsapp.com/send?phone=62811599594&text=Hello, more information!" target="_blank" className="contact-button mb-2">
+                  Contact us
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#07960c" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M7 7h10v10"></path>
+                    <path d="M7 17 17 7"></path>
                   </svg>
-
-                  <h3 className="font-semibold font-lexend">Whatsapp</h3>
-                  <span className="contact-card-data mb-2">0811-599-594</span>
-
-                  <a href="https://api.whatsapp.com/send?phone=62811599594&text=Hello, more information!" target="_blank" className="contact-button mb-2">
-                    Contact us
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#07960c" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M7 7h10v10"></path>
-                      <path d="M7 17 17 7"></path>
-                    </svg>
-                  </a>
-                </div>
+                </a>
               </div>
             </div>
-
-            <div className="contact-content">
-              <form ref={form} onSubmit={sendEmail} className="contact-form">
-                <div className="contact-form-div">
-                  <label htmlFor="" className="contact-form-tag">
-                    Name
-                  </label>
-                  <input type="text" name="name" className="contact-form-input" placeholder="Insert your name" />
-                </div>
-
-                <div className="contact-form-div">
-                  <label htmlFor="" className="contact-form-tag">
-                    Email
-                  </label>
-                  <input type="email" name="email" className="contact-form-input" placeholder="Insert your email" />
-                </div>
-
-                <div className="contact-form-div contact-form-area">
-                  <label htmlFor="" className="contact-form-tag">
-                    Message
-                  </label>
-                  <textarea name="message" cols="30" rows="10" className="contact-form-input" placeholder="Write your message here!"></textarea>
-                </div>
-
-                <button type="submit" className="cursor-pointer inline-flex items-center gap-2 bg-green-700 hover:bg-green-800 text-white font-semibold py-2 px-4 rounded transition">
-                  Send
-                </button>
-              </form>
-            </div>
           </div>
 
-          <div className="mt-12 mb-24">
-            <h1 className="text-3xl font-bold text-center mb-12">Or Visit Us Here</h1>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.869995460516!2d116.87205300000001!3d-1.2492467999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df1468cad2c11c9%3A0x227c6c91d6aceec7!2sCV.%20Rizki%20Bangun!5e0!3m2!1sid!2sid!4v1749638184129!5m2!1sid!2sid"
-              loading="lazy"
-              title="CV. Rizki Bangun"
-              className="shadow-xl rounded w-3xl h-96 mx-auto"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
+          <div className="contact-content">
+            <form ref={form} onSubmit={sendEmail} className="contact-form">
+              <div className="contact-form-div">
+                <label htmlFor="" className="contact-form-tag">
+                  Name
+                </label>
+                <input type="text" name="name" className="contact-form-input" placeholder="Insert your name" />
+              </div>
+
+              <div className="contact-form-div">
+                <label htmlFor="" className="contact-form-tag">
+                  Email
+                </label>
+                <input type="email" name="email" className="contact-form-input" placeholder="Insert your email" />
+              </div>
+
+              <div className="contact-form-div contact-form-area">
+                <label htmlFor="" className="contact-form-tag">
+                  Message
+                </label>
+                <textarea name="message" cols="30" rows="10" className="contact-form-input" placeholder="Write your message here!"></textarea>
+              </div>
+
+              <button type="submit" className="cursor-pointer inline-flex items-center gap-2 bg-green-700 hover:bg-green-800 text-white font-semibold py-2 px-4 rounded transition">
+                Send
+              </button>
+            </form>
           </div>
+        </div>
+
+        <div className="mt-12 mb-24">
+          <h1 className="text-3xl font-bold text-center mb-12">Or Visit Us Here</h1>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.869995460516!2d116.87205300000001!3d-1.2492467999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df1468cad2c11c9%3A0x227c6c91d6aceec7!2sCV.%20Rizki%20Bangun!5e0!3m2!1sid!2sid!4v1749638184129!5m2!1sid!2sid"
+            loading="lazy"
+            title="CV. Rizki Bangun"
+            className="shadow-xl rounded w-3xl h-96 mx-auto"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </>
