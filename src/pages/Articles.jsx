52cb3514432e9090ce5b5506df006d32bd92ca0e
faiz832 <faqodefazz@gmail.com>
2025-06-11 20:24:48 +0700

    change pattern

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import articles from "../data/articles";
-import Pattern from "../assets/icons/pattern.svg";
+import Pattern2 from "../assets/icons/pattern2.svg";
+import Pattern3 from "../assets/icons/pattern3.svg";
 
 export default function Articles() {
   return (
     <>
       <div className="relative overflow-hidden">
-        <img src={Pattern} alt="" className="absolute right-0 top-0 w-40 h-auto -rotate-45 -z-1 opacity-10" />
-        <img src={Pattern} alt="" className="absolute left-0 top-0 w-40 h-auto rotate-45 -z-1 opacity-10" />
+        <img src={Pattern3} alt="" className="absolute right-0 top-12 w-40 opacity-20" />
+        <img src={Pattern2} alt="" className="absolute left-0 top-12 w-40 opacity-20" />
+
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-6xl text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-700 to-green-400 my-12">Articles</h1>
           <p className="w-full lg:w-2/4 mx-auto text-center text-lg">Explore expert insights and industry knowledge about sandblasting, surface preparation, and equipment maintenance.</p>
