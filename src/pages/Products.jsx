b11c7753f55aa22aacbe0c8953b5f8c5d1235268
faiz832 <faqodefazz@gmail.com>
2025-06-11 20:25:32 +0700

    product update

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -14,7 +14,6 @@ export default function Products() {
 
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-6xl text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-700 to-green-400 my-12">Products</h1>
-          <p className="w-full lg:w-2/4 mx-auto text-center text-lg">Explore expert insights and industry knowledge about sandblasting, surface preparation, and equipment maintenance.</p>
 
           <div className="mt-12 my-20">
             <div className="flex justify-center mb-8 space-x-4">
